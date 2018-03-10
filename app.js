7cfc2e8571b2883b774318014c2aa5203090758a
Jeff Wong <jeffmjwong@gmail.com>
2018-03-10 14:03:48 +1000

    firstElementChild and lastElementChild

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const listUl = listDiv.querySelector('ul');
 const listItems = listUl.children;
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
+const firstListItem = listUl.firstElementChild;
+const lastListItem = listUl.lastElementChild;
+
+firstListItem.style.backgroundColor = "lightskyblue";
+lastListItem.style.backgroundColor = "lightsteelblue";
 
 function attachListItemButtons(li) {
   let up = document.createElement('button');
