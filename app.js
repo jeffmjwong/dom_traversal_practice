049a3aee5cdb86440ffaf4c873bcaa2a8cbdd4b2
Jeff Wong <jeffmjwong@gmail.com>
2018-03-10 13:21:38 +1000

    Traversing previous/nextElementSibling and element.children

--- app.js
+++ app.js
@@ -4,8 +4,30 @@ const descriptionInput = document.querySelector('input.description');
 const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
 const listUl = listDiv.querySelector('ul');
+const listItems = listUl.children;
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
+
+function attachListItemButtons(li) {
+  let up = document.createElement('button');
+  up.className = "up";
+  up.textContent = "Up";
+  li.appendChild(up);
+
+  let down = document.createElement('button');
+  down.className = "down";
+  down.textContent = "Down";
+  li.appendChild(down);
+
+  let remove = document.createElement('button');
+  remove.className = "remove";
+  remove.textContent = "Remove";
+  li.appendChild(remove);
+}
+
+for (let i = 0; i < listItems.length; i++) {
+  attachListItemButtons(listItems[i]);
+}
 
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
@@ -26,13 +48,30 @@ addItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.createElement('li');
   li.textContent = addItemInput.value;
+  attachListItemButtons(li);
   ul.appendChild(li);
   addItemInput.value = '';
 });
 
 listUl.addEventListener('click', (evt) => {
   if (evt.target.tagName === "BUTTON") {
-    let li = evt.target.parentNode;
-    listUl.removeChild(li);
+    if (evt.target.className === "remove") {
+      let li = evt.target.parentNode;
+      listUl.removeChild(li);
+    };
+    if (evt.target.className === "up") {
+      let li = evt.target.parentNode;
+      let prevLi = li.previousElementSibling;
+      if (prevLi !== null) {
+        listUl.insertBefore(li, prevLi);
+      };
+    };
+    if (evt.target.className === "down") {
+      let li = evt.target.parentNode;
+      let nextLi = li.nextElementSibling;
+      if (nextLi !== null) {
+        listUl.insertBefore(nextLi, li);
+      };
+    };
   };
 });
